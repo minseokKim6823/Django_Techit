0cc18aa79c70e287484b6f72ecd0627b9712435d
minseokKim6823 <alstjr6823@naver.com>
2023-06-01 15:29:02 +0900

    계산기ing

--- first-django/demos/views.py
+++ first-django/demos/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def calculator(request):
-    return HttpResponse("계산기 기능 구현")
+    # return HttpResponse("계산기 기능 구현")
+    return render(request, 'calculator.html')
